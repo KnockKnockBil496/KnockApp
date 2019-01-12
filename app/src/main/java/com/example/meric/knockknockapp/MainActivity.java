2d9278b44f5dcbf0a0066b5d6c106c1c48464454
unknown <beyza.olmez96@gmail.com>
2019-01-12 18:49:28 +0300

    Camera usage is available

--- app/src/main/java/com/example/meric/knockknockapp/MainActivity.java
+++ app/src/main/java/com/example/meric/knockknockapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.meric.knockknockapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +11,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int Camera_Req = 9999;
+    Button addNew1;
+    ImageView foto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +36,31 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+        foto = findViewById(R.id.imageView);
+        addNew1 = findViewById(R.id.addNew);
+
+        addNew1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent kamera=new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // Resim çekme isteği
+                startActivityForResult(kamera,Camera_Req);
+            }
+        });
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==Camera_Req){
+            Bitmap image=(Bitmap)data.getExtras().get("data");//Çekilen resim id olarak bitmap şeklinde alındı ve imageview'e atandı
+            foto.setImageBitmap(image);
+        }
+
+
+        // super.onActivityResult(requestCode, resultCode, data);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
