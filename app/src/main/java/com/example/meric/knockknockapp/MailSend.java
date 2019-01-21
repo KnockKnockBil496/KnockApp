b8e6afc72dd6130595fb9ec9b99c817ca70f3e35
maegwyn <burakshn96@gmail.com>
2019-01-21 13:29:22 +0300

    #3 User can now send email to any mail address via push of a button.

---
+++ app/src/main/java/com/example/meric/knockknockapp/MailSend.java
@@ -0,0 +1,112 @@
+package com.example.meric.knockknockapp;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.util.Config;
+import android.widget.Toast;
+
+import java.util.Date;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+//Class is extending AsyncTask because this class is going to perform a networking operation
+public class MailSend extends AsyncTask<Void,Void,Void> {
+
+    //Declaring Variables
+    private Context context;
+    private Session session;
+
+    //Information to send email
+    private String email;
+    private String subject;
+    private String message;
+
+    //Progressdialog to show while sending email
+    private ProgressDialog progressDialog;
+
+    //Class Constructor
+    public MailSend(Context context, String email, String subject, String message) {
+        //Initializing variables
+        this.context = context;
+        this.email = email;
+        this.subject = subject;
+        this.message = message;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        //Showing progress dialog while sending email
+        progressDialog = ProgressDialog.show(context, "Sending message", "Please wait...", false, false);
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        //Dismissing the progress dialog
+        progressDialog.dismiss();
+        //Showing a success message
+        Toast.makeText(context, "Message Sent", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        final String username = "knockknockapplication@gmail.com";
+        final String password = "12341234)a";
+
+        Properties props = System.getProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        //Creating a new session
+        session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    //Authenticating the password
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("knockknockapplication@gmail.com", "12341234)a");
+                    }
+                });
+
+        try {
+            //Creating MimeMessage object
+            MimeMessage mm = new MimeMessage(session);
+
+            //Setting sender address
+            mm.setFrom(new InternetAddress("knockknockapplication@gmail.com"));
+            //Adding receiver
+            mm.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+            //Adding subject
+            mm.setSubject(subject);
+            //Adding message
+            mm.setText(message);
+
+            //Sending email
+            Transport.send(mm);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
