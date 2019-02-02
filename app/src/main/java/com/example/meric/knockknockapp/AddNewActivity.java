bb05b6b61a3a3328bb0865e1c755004d22a29bf9
Meric <mericbalgamis@gmail.com>
2019-02-03 01:57:29 +0300

    Trying to entegrate face training to app

--- app/src/main/java/com/example/meric/knockknockapp/AddNewActivity.java
+++ app/src/main/java/com/example/meric/knockknockapp/AddNewActivity.java
@@ -1,16 +1,21 @@
 package com.example.meric.knockknockapp;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -68,23 +73,13 @@ public class AddNewActivity extends AppCompatActivity implements CameraBridgeVie
     public static final int Camera_Req = 9999;
     public static boolean save=false;
 
-    private static final String TAG = "FaceTracker";
-    private CameraSource mCameraSource = null;
-    private CameraSourcePreview mPreview;
-    private GraphicOverlay mGraphicOverlay;
-    private static final int RC_HANDLE_GMS = 9001;
-    // permission request codes need to be < 256
-    private static final int RC_HANDLE_CAMERA_PERM = 2;
-
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
-        //mPreview yerine foto kullan
-        mGraphicOverlay = (GraphicOverlay) findViewById(R.id.capture);
 
-        mStorage = FirebaseStorage.getInstance().getReference();
+//        mStorage = FirebaseStorage.getInstance().getReference();
         setContentView(R.layout.activity_add_new);
         foto = findViewById(R.id.imageView);//küçük resim olarak gösterildiği yer
         captr = findViewById(R.id.capture);//fotoğraf çekme butonu
@@ -93,16 +88,17 @@ public class AddNewActivity extends AppCompatActivity implements CameraBridgeVie
         cancel = findViewById(R.id.cancel_btn);//yeni kişi ekleme sayfasından çıkılır
 
 
-        captr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent kamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // Resim çekme isteği
-                Toast.makeText(AddNewActivity.this,"Kamera açıldı!",Toast.LENGTH_LONG).show();
-                startActivityForResult(kamera, Camera_Req);
-
-            }
-        });
+//        captr.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//
+//                Intent kamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // Resim çekme isteği
+//                Toast.makeText(AddNewActivity.this,"Kamera açıldı!",Toast.LENGTH_LONG).show();
+//                startActivityForResult(kamera, Camera_Req);
+//
+//            }
+//        });
 
         kaydet.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -125,6 +121,14 @@ public class AddNewActivity extends AppCompatActivity implements CameraBridgeVie
             }
         });
     }
+
+    public void detectFace(View view){
+
+        //Intent intent = new Intent(this,VideoFaceDetectionActivity.class);
+        //startActivity(intent);
+
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
@@ -162,7 +166,6 @@ public class AddNewActivity extends AppCompatActivity implements CameraBridgeVie
 
     private void createImageFile( String labelOfImage) throws IOException {//return type was File
         // Create an image file name
-
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = labelOfImage+"_" + timeStamp + "_"; // kullanıcının girdiği isim fotoğrafın adı olacak
         File storageDir = Environment.getExternalStoragePublicDirectory(DIRECTORY_DCIM);
@@ -230,57 +233,6 @@ public class AddNewActivity extends AppCompatActivity implements CameraBridgeVie
         foto.setImageBitmap(bitmap);
     }
 
-    private void createCameraSource() {
-
-        Context context = getApplicationContext();
-        FaceDetector detector = new FaceDetector.Builder(context)
-                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
-                .build();
-
-        detector.setProcessor(
-                new MultiProcessor.Builder<>(new GraphicFaceTrackerFactory())
-                        .build());
-
-        if (!detector.isOperational()) {
-            // Note: The first time that an app using face API is installed on a device, GMS will
-            // download a native library to the device in order to do detection.  Usually this
-            // completes before the app is run for the first time.  But if that download has not yet
-            // completed, then the above call will not detect any faces.
-            //
-            // isOperational() can be used to check if the required native library is currently
-            // available.  The detector will automatically become operational once the library
-            // download completes on device.
-            Log.w(TAG, "Face detector dependencies are not yet available.");
-        }
-
-        mCameraSource = new CameraSource.Builder(context, detector)
-                .setRequestedPreviewSize(640, 480)
-                .setFacing(CameraSource.CAMERA_FACING_BACK)
-                .setRequestedFps(30.0f)
-                .build();
-    }
-
-    private void startCameraSource() {
-
-        // check that the device has play services available.
-        int code = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(
-                getApplicationContext());
-        if (code != ConnectionResult.SUCCESS) {
-            Dialog dlg =
-                    GoogleApiAvailability.getInstance().getErrorDialog(this, code, RC_HANDLE_GMS);
-            dlg.show();
-        }
-
-        if (mCameraSource != null) {
-            try {
-                foto.start(mCameraSource, mGraphicOverlay);
-            } catch (IOException e) {
-                Log.e(TAG, "Unable to start camera source.", e);
-                mCameraSource.release();
-                mCameraSource = null;
-            }
-        }
-    }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
@@ -297,57 +249,5 @@ public class AddNewActivity extends AppCompatActivity implements CameraBridgeVie
         return null;
     }
 
-    private class GraphicFaceTrackerFactory implements MultiProcessor.Factory<Face> {
-        @Override
-        public Tracker<Face> create(Face face) {
-            return new GraphicFaceTracker(mGraphicOverlay);
-        }
-    }
-
-    private class GraphicFaceTracker extends Tracker<Face> {
-        private GraphicOverlay mOverlay;
-        private FaceGraphic mFaceGraphic;
-
-        GraphicFaceTracker(GraphicOverlay overlay) {
-            mOverlay = overlay;
-            mFaceGraphic = new FaceGraphic(overlay);
-        }
-
-        /**
-         * Start tracking the detected face instance within the face overlay.
-         */
-        @Override
-        public void onNewItem(int faceId, Face item) {
-            mFaceGraphic.setId(faceId);
-        }
-
-        /**
-         * Update the position/characteristics of the face within the overlay.
-         */
-        @Override
-        public void onUpdate(FaceDetector.Detections<Face> detectionResults, Face face) {
-            mOverlay.add(mFaceGraphic);
-            mFaceGraphic.updateFace(face);
-        }
-
-        /**
-         * Hide the graphic when the corresponding face was not detected.  This can happen for
-         * intermediate frames temporarily (e.g., if the face was momentarily blocked from
-         * view).
-         */
-        @Override
-        public void onMissing(FaceDetector.Detections<Face> detectionResults) {
-            mOverlay.remove(mFaceGraphic);
-        }
-
-        /**
-         * Called when the face is assumed to be gone for good. Remove the graphic annotation from
-         * the overlay.
-         */
-        @Override
-        public void onDone() {
-            mOverlay.remove(mFaceGraphic);
-        }
-    }
 
 }//class
