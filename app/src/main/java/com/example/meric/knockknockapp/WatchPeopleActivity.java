8f5896aefe8f479878556736756f47406964a825
unknown <beyza.olmez96@gmail.com>
2019-02-03 18:27:15 +0300

    Faces on the images can detected and marked.

--- app/src/main/java/com/example/meric/knockknockapp/WatchPeopleActivity.java
+++ app/src/main/java/com/example/meric/knockknockapp/WatchPeopleActivity.java
@@ -1,6 +1,7 @@
 package com.example.meric.knockknockapp;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +14,20 @@ import org.opencv.android.InstallCallbackInterface;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.SparseArray;
+import android.widget.ImageView;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
 
 public class WatchPeopleActivity  extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
@@ -47,7 +62,8 @@ public class WatchPeopleActivity  extends AppCompatActivity implements CameraBri
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.camera_view);
+        //setContentView(R.layout.camera_view);
+        setContentView(R.layout.face_rec);
 
         if(!OpenCVLoader.initDebug())
         {
@@ -58,7 +74,7 @@ public class WatchPeopleActivity  extends AppCompatActivity implements CameraBri
 
 
        // cameraManager.enableView();
-        cameraManager = findViewById(R.id.camera);
+      /*  cameraManager = findViewById(R.id.camera);
         cameraManager.setVisibility(View.VISIBLE);
         cameraManager.setCvCameraViewListener(this);
 
@@ -72,7 +88,55 @@ public class WatchPeopleActivity  extends AppCompatActivity implements CameraBri
             }
         });
 
+*/
 
+
+        //   --- Kamera açılmadığı için bir foto üstünde denedim ---
+
+
+        // Fotoğraf yükleyip fotodaki yüzleri kare içine alana kod;
+
+        ImageView myImageView = (ImageView) findViewById(R.id.imgview);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inMutable = true;
+         Bitmap myBitmap = BitmapFactory.decodeResource(
+                    getApplicationContext().getResources(),
+                    R.drawable.test2,
+                    options);
+            //okunacak dosya adı test2
+
+
+        FaceDetector faceDetector= new FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(true).build();
+           /* if (!faceDetector.isOperational()) {
+                new AlertDialog.Builder(.getContext()).setMessage("Could not set up the face detector!").show();
+                return;
+            }*/
+
+        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();//görüntü bitmap olmalı
+
+        SparseArray<Face> faces = faceDetector.detect(frame);
+
+        Paint myRectPaint = new Paint();
+        myRectPaint.setStrokeWidth(5);
+        myRectPaint.setColor(Color.RED);
+        myRectPaint.setStyle(Paint.Style.STROKE);
+
+        Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
+        Canvas tempCanvas = new Canvas(tempBitmap);
+        tempCanvas.drawBitmap(myBitmap, 0, 0, null);
+
+
+        // Yüz'ün etrafında kırmızı kare çizmek için;
+
+        for(int i=0; i<faces.size(); i++) {
+            Face thisFace = faces.valueAt(i);
+            float x1 = thisFace.getPosition().x;
+            float y1 = thisFace.getPosition().y;
+            float x2 = x1 + thisFace.getWidth();
+            float y2 = y1 + thisFace.getHeight();
+            tempCanvas.drawRoundRect(new RectF(x1, y1, x2, y2), 2, 2, myRectPaint);
+        }
+        myImageView.setImageDrawable(new BitmapDrawable(getResources(),tempBitmap));
 
     }
 
